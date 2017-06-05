f9b579a8a224b9f7d21ed31df1c33c8570ac97c2
changlor <958142428@qq.com>
2017-06-05 15:51:57 +0800

    增加css3过渡动画支持
    你问我滋瓷不滋瓷，我说我当然是滋瓷的

--- transition.js
+++ transition.js
@@ -14,6 +14,46 @@ var Transition = (function() {
         this.state = {};
     };
 
+    _Transition.prototype.cssParser = function (css) {
+        this.cssSelector = [];
+        for (var key in css) {
+            this.cssSelector.push({
+                key: `CFOSTransition-${key}`,
+                value: css[key],
+            });
+        }
+    }
+
+    _Transition.prototype.cssSetter = function () {
+        this.classNode = document.createAttribute('class');
+        this.classes = { arr: [], obj: {} };
+        elReplace.setAttributeNode(this.classNode);
+    }
+
+    _Transition.prototype.cssRender = function () {
+        this.style = [];
+        for (var key in this.cssSelector) {
+            var cssKey = this.cssSelector[key].key;
+            var cssValue = JSON.stringify(this.cssSelector[key].value);
+            cssValue = cssValue.replace(/,/g, ';');
+            cssValue = cssValue.replace(/"/g, '');
+            this.style.push({ cssKey: cssKey, cssValue: cssValue })
+        }
+        var style = '';
+        for (var i = 0; i < this.style.length; i++) {
+            style = `${style}.${this.style[i].cssKey}${this.style[i].cssValue}`
+        }
+        var styleNode = document.createElement('style');
+        styleNode.innerHTML = style;
+        document.head.appendChild(styleNode)
+    }
+
+    _Transition.prototype.updateClass = function (className) {
+        var index = this.classes.arr = [className];
+        this.classes.obj[className] = index;
+        this.classNode.value = this.classes.arr.join(' ');
+    }
+
     _Transition.prototype.bindVariableListener = function (watch) {
         Object.defineProperty(this.state, watch.variable, {
             set: function (val) {
@@ -24,27 +64,34 @@ var Transition = (function() {
 
     var _transition = new _Transition();
 
-    var showListener ={
+    var showListener = {
         variable: variableShow,
         func: function (val) {
             elReplace.style.display = val
-            ? 'block'
-            : 'none';
+            ? _transition.updateClass(_transition.cssSelector[1].key)
+            : _transition.updateClass(_transition.cssSelector[0].key);
         }
     }
 
     _transition.bindVariableListener(showListener);
 
-    console.log(_transition.state.isShow = true)
-
     var Transition = function () {
         this.state = {};
         this.state[variableShow] = true;
+    }
+
+    Transition.prototype.cssAnimation = function (css) {
+        _transition.cssParser(css);
+        _transition.cssSetter();
+        _transition.cssRender();
+        var cssSelector = _transition.cssSelector;
+        
     }
     
     Transition.prototype.setState = function (object, changes) {
         object[variableShow] = changes.isShow;
         _transition.state[variableShow] = changes.isShow;
     }
+
     return Transition;
 })()
