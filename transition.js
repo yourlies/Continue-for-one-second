a5c92b4c9e829fa8a8179a9b548e734db9ae0ad2
changlor <958142428@qq.com>
2017-06-06 14:44:51 +0800

    规范化了一些逻辑，删除了不必要的操作和意味不明的变量名

--- transition.js
+++ transition.js
@@ -11,69 +11,89 @@ var Transition = (function() {
 
     var _Transition = function () {
         this.state = {};
-        this.tasks = [];
-        this.tasksLength = 0;
+        this.tasks = []; this.tasksCount = 0;
+
+        this.style = [];
+        this.scopedClasses = [];
+        this.sortedClasses = {
+            classesIndexToKey: [],
+            classesKeyToIndex: {},
+            classesRawKeyToIndex: {},
+            classes: []
+        };
+
+        this.classNode = document.createAttribute('class');
+        elReplace.setAttributeNode(this.classNode);
+        this.styleNode = document.createElement('style');
+        document.head.appendChild(this.styleNode)
+
     };
 
-    _Transition.prototype.cssParser = function (css) {
-        this.cssSelector = [];
-        for (var key in css) {
-            this.cssSelector.push({
+    _Transition.prototype.classesCreator = function (classes) {
+        for (var key in classes) {
+            this.scopedClasses.push({
+                rawKey: key,
                 key: `CFOSTransition-${key}`,
-                value: css[key],
+                value: classes[key],
             });
         }
     }
 
-    _Transition.prototype.cssSetter = function () {
-        this.classNode = document.createAttribute('class');
-        this.classes = { arr: [], obj: {} };
-        elReplace.setAttributeNode(this.classNode);
-    }
-
-    _Transition.prototype.cssRender = function () {
-        this.style = [];
-        for (var key in this.cssSelector) {
-            var cssKey = this.cssSelector[key].key;
-            var cssValue = JSON.stringify(this.cssSelector[key].value);
-            cssValue = cssValue.replace(/,/g, ';');
-            cssValue = cssValue.replace(/"/g, '');
-            this.style.push({ cssKey: cssKey, cssValue: cssValue })
+    _Transition.prototype.classesSorter = function () {
+        for (var i = 0; i < this.scopedClasses.length; i++) {
+            var index = this.sortedClasses.classesIndexToKey.push(this.scopedClasses[i].key);
+            this.sortedClasses.classesRawKeyToIndex[this.scopedClasses[i].rawKey] = index - 1;
+            this.sortedClasses.classesKeyToIndex[this.scopedClasses[i].key] = index - 1;
+            this.sortedClasses.classes.push('');
         }
-        var style = '';
-        for (var i = 0; i < this.style.length; i++) {
-            style = `${style}.${this.style[i].cssKey}${this.style[i].cssValue}`
+    }
+
+    _Transition.prototype.classesRender = function () {
+        for (var i = 0; i < this.scopedClasses.length; i++) {
+            var className = this.scopedClasses[i].key;
+            var classValue = JSON.stringify(this.scopedClasses[i].value);
+            classValue = classValue.replace(/,/g, ';');
+            classValue = classValue.replace(/"/g, '');
+            this.style.push(`.${className}${classValue}`)
         }
-        var styleNode = document.createElement('style');
-        styleNode.innerHTML = style;
-        document.head.appendChild(styleNode)
+
+        this.styleNode.innerHTML = this.style.join('');
     }
 
-    _Transition.prototype.updateClass = function (className) {
-        var index = this.classes.arr = [className, _transition.cssSelector[1].key];
-        this.classes.obj[className] = index;
-        this.classNode.value = this.classes.arr.join(' ');
+    _Transition.prototype.updateClass = function () {
+        var classes = this.sortedClasses.classes.join(' ');
+        classes = classes.trim();
+        classes = classes.replace(/[ ]+/g, ' ');
+        this.classNode.value = classes;
     }
 
-    _Transition.prototype.tasksQueue = function (className) {
-        this.tasksLength++;
-        this.updateClass(className);
+    _Transition.prototype.removeClass = function (className) {
+        var index = this.sortedClasses.classesKeyToIndex[className];
+        this.sortedClasses.classes[index] = '';
+        this.updateClass();
+    }
+
+    _Transition.prototype.insertClass = function (className) {
+        var index = this.sortedClasses.classesKeyToIndex[className];
+        this.sortedClasses.classes[index] = className;
+        this.updateClass();
+    }
+
+    _Transition.prototype.taskCreator = function (task) {
         var _this = this;
-        var task = function () {
-            return new Promise (function (resolve) {
-                setTimeout(function () {
-                    resolve();
-                    _this.tasksLength--;
-                }, 1000)
+        return function () {
+            new Promise (function (resolve) {
+                task(_this, resolve);
             })
-        };
+        }
+    }
+
+    _Transition.prototype.tasksQueue = function (task, callback) {
+        var _this = this;
+        this.tasksCount++;
         this.tasks.push(task());
         Promise.all(this.tasks).then(function () {
-            if (_this.tasksLength == 0) {
-                className == 'CFOSTransition-fade'
-                    ? elReplace.style.display = 'none'
-                    : elReplace.style.display = 'block';
-            }
+            callback(_this);
         });
     }
 
@@ -90,9 +110,41 @@ var Transition = (function() {
     var showListener = {
         variable: variableShow,
         func: function (val) {
-            elReplace.style.display = val
-            ? _transition.tasksQueue(_transition.cssSelector[2].key)
-            : _transition.tasksQueue(_transition.cssSelector[0].key);
+            var fadeIndex = _transition.sortedClasses.classesRawKeyToIndex['fade'];
+            var showIndex = _transition.sortedClasses.classesRawKeyToIndex['show'];
+            var appearIndex = _transition.sortedClasses.classesRawKeyToIndex['appear'];
+            var fadeClass = _transition.sortedClasses.classesIndexToKey[fadeIndex];
+            var showClass = _transition.sortedClasses.classesIndexToKey[showIndex];
+            var appearClass = _transition.sortedClasses.classesIndexToKey[appearIndex];
+
+            var fade = function (_, resolve) {
+                _transition.insertClass(appearClass);
+                _transition.removeClass(showClass);
+                _transition.insertClass(fadeClass);
+                resolve();
+
+            };
+            var show = function (_, resolve) {
+                _transition.insertClass(appearClass);
+                _transition.insertClass(showClass);
+                _transition.removeClass(fadeClass);
+                resolve();
+            }
+
+            var task = elReplace.style.display = val
+                ? _transition.taskCreator(show)
+                : _transition.taskCreator(fade);
+
+            _transition.tasksQueue(task, function (transition) {
+                setTimeout(function () {
+                    transition.tasksCount--;
+                    if (transition.tasksCount == 0) {
+                        elReplace.style.display = val
+                            ? 'block'
+                            : 'none';
+                    }
+                }, 1000);
+            });
         }
     }
 
@@ -104,10 +156,9 @@ var Transition = (function() {
     }
 
     Transition.prototype.cssAnimation = function (css) {
-        _transition.cssParser(css);
-        _transition.cssSetter();
-        _transition.cssRender();
-        var cssSelector = _transition.cssSelector;
+        _transition.classesCreator(css);
+        _transition.classesSorter();
+        _transition.classesRender();
     }
     
     Transition.prototype.setState = function (object, changes) {
